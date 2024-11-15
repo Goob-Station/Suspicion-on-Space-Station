32f82f3cecdec51af176d65c1a98ca27b076cc88
DrSmugleaf <drsmugleaf@gmail.com>
2024-11-14 21:24:04 -0800

    Fix revolvers having gun damage multipliers applied twice

--- Content.Shared/Weapons/Ranged/Systems/SharedGunSystem.cs
+++ Content.Shared/Weapons/Ranged/Systems/SharedGunSystem.cs
@@ -535,8 +535,7 @@ public abstract partial class SharedGunSystem : EntitySystem
                 case AmmoComponent newAmmo:
                     if (_netManager.IsServer || GunPrediction)
                     {
-                        shotProjectiles.Add(ent!.Value);
-                        CreateAndFireProjectiles(ent.Value, newAmmo);
+                        CreateAndFireProjectiles(ent!.Value, newAmmo);
                     }
                     else
                     {
